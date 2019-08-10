05a5b580705fe2d851f9f3de6f68fede0e5a7508
hmubashar <hmubashar639@gmail.com>
2019-08-10 12:51:50 +0500

    mubashar branch

--- src/components/table.js
+++ src/components/table.js
@@ -1,48 +1,51 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import TableDragSelect from "react-table-drag-select";
 import "react-table-drag-select/style.css";
 import "../App.css"
 class Table extends Component {
   state = {
     cells: [
-        [false, false, false, false, false, false, false, false, false, false, false, false , false, false, false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false, false, false , false, false, false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false, false, false , false, false, false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false, false, false , false, false, false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false, false, false , false, false, false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false, false, false , false, false, false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false, false, false , false, false, false, false, false, false, false, false, false, false, false, false],
-        [false, false, false, false, false, false, false, false, false, false, false, false , false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+      [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
 
     ],
-  
-      Sunday: [],
-      Monday: [],
-      Tuesday: [],
-      Wednesday: [],
-      Thursday: [],
-      Friday: [],
-      Saturday: [],
-  
+
+    Sunday: [],
+    Monday: [],
+    Tuesday: [],
+    Wednesday: [],
+    Thursday: [],
+    Friday: [],
+    Saturday: [],
+
   };
-getData =(e) => {
-  e.preventDefault()
-  if (this.state.cells[1][1] === true){ this.setState({ Sunday: [...this.state.Sunday, "00"] })}
-  if (this.state.cells[1][2] === true){ this.setState({ Sunday: [...this.state.Sunday, "01"] })}
-  if (this.state.cells[1][3] === true){ this.setState({ Sunday: [...this.state.Sunday, "02"] })}
-  if (this.state.cells[1][4] === true){ this.setState({ Sunday: [...this.state.Sunday, "03"] })}
-  if (this.state.cells[1][5] === true){ this.setState({ Sunday: [...this.state.Sunday, "04"] })}
-  console.log(this.state)
-}
-  render = () =>
-  <div>
-
-
-    <TableDragSelect
-      value={this.state.cells}
-      onChange={cells => this.setState({ cells })}
-    >
-        <tr>
+  getData = (e) => {
+    let Sunday= []
+    if (this.state.cells[1][1] === true) {  Sunday.push("00") }
+    if (this.state.cells[1][2] === true) { Sunday.push( "01") }
+    if (this.state.cells[1][3] === true) { Sunday.push( "02") }
+    if (this.state.cells[1][4] === true) { Sunday.push( "03") }
+    if (this.state.cells[1][5] === true) { Sunday.push( "04") }
+    this.setState({
+      Sunday
+    },()=>{console.log(this.state.Sunday)})
+    console.log(this.state.Sunday)
+  }
+  render() {
+  console.log(this.state.cells)
+    return (
+      <div>
+        <TableDragSelect
+          value={this.state.cells}
+          onChange={cells => this.setState({ cells },()=>{this.getData()})}
+        >
+          <tr>
             <th disabled className="h-Day1">Day</th>
             <th disabled className="h-row">00</th>
             <th disabled className="h-row">01</th>
@@ -67,193 +70,195 @@ getData =(e) => {
             <th disabled className="h-row">21</th>
             <th disabled className="h-row">22</th>
             <th disabled className="h-row">23</th>
-        </tr>
-      <tr>
-        <td disabled className="c-day">Sunday</td>
-        < td id="sunday00" name="seleted"  className="my-td" />
-        < td id="sunday00" name="seleted" className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-      </tr>
-      <tr>
-      <td disabled className="c-day2">Monday</td>
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-      </tr>
-      <tr>
-      <td disabled className="c-day">Tuesday</td>
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-      </tr>
-      <tr>
-      <td disabled className="c-day2">Wednesday</td>
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-      </tr>
-      <tr>
-      <td disabled className="c-day">Thursday</td>
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-      </tr>
-      <tr>
-      <td disabled className="c-day2">Friday</td>
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-     
-      </tr>
-      <tr>
-      <td disabled className="c-day">Saturday</td>
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        < td className="my-td" />
-        
-      </tr>
-    </TableDragSelect>
-    <button onClick={this.getData}>get data</button>
-    </div>
+          </tr>
+          <tr>
+            <td disabled className="c-day">Sunday</td>
+            < td id="sunday00" name="seleted" className="my-td" />
+            < td id="sunday00" name="seleted" className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+          </tr>
+          <tr>
+            <td disabled className="c-day2">Monday</td>
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+          </tr>
+          <tr>
+            <td disabled className="c-day">Tuesday</td>
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+          </tr>
+          <tr>
+            <td disabled className="c-day2">Wednesday</td>
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+          </tr>
+          <tr>
+            <td disabled className="c-day">Thursday</td>
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+          </tr>
+          <tr>
+            <td disabled className="c-day2">Friday</td>
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+
+          </tr>
+          <tr>
+            <td disabled className="c-day">Saturday</td>
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+            < td className="my-td" />
+
+          </tr>
+        </TableDragSelect>
+        <button onClick={this.getData}>get data</button>
+      </div>
+    )
+  }
 }
 export default Table;
